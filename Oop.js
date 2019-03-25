9a87d3f187d71a35b13677130e337b618d73a33c
motasem6236 <motasem6236@gmail.com>
2019-03-25 10:02:32 +0200

    oop

--- Oop.js
+++ Oop.js
@@ -6,25 +6,34 @@ function randInt(n) {
     return Math.floor(Math.random() * (n + 1));
 }
 
-function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
-  }
+function makeGame(){
+
+  var game = {}
+   upper = upperbound;
+  rand = randInt(upper);
+  counter = 0;
+  game.guessMyNumber = guessMyNumber;
+  game.giveUp = giveUp;
+  game.numOfGuesses = numOfGuesses;
+  return game;
 }
+
+ var guessMyNumber = function (n){
+  counter++;
+      if (n > this.upper) {
+        return "Out of bounds! Please try a number between 0 and " + this.upperbound + ".";
+      } else if (n === this.rand) {
+        return "You guessed my number!";
+      }
+ };
+
+var giveUp = function(){
+  return this.rand;
+};
+
+var numOfGuesses = function (){
+  return this.counter;
+}
+
+
+var oldGame = makeGame()
